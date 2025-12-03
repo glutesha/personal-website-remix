5eb25689a1be2cd8eca19a382b8cc321dbf1c2a1
glutesha <mail@glutesha.me>
2025-12-03 18:27:40 +0300

    Edited a breakpoint, improved webring responsiveness

--- src/components/widgets/react/Webring.tsx
+++ src/components/widgets/react/Webring.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 interface entry {
     name: string;
@@ -9,9 +9,8 @@ export const Webring = () => {
     const [prev, setPrev] = useState<entry | null>(null);
     const [next, setNext] = useState<entry | null>(null);
 
-    const [visible, setVisible] = useState(false);
-    const [isAtBottom, setAtBottom] = useState(false);
-    const [lastScrollY, setLastScrollY] = useState(0);
+    const [scrolled, setScrolled] = useState(false);
+    const [bottom, setBottom] = useState(false);
 
     useEffect(() => {
         fetch("https://webring.otomir23.me/30/data")
@@ -32,14 +31,8 @@ export const Webring = () => {
                 document.documentElement.offsetHeight
             );
 
-            setAtBottom(scrollY + innerH >= docHeight - 20);    
-
-            if (scrollY > lastScrollY && scrollY > 20 && !isAtBottom) {
-                setVisible(true);
-            } else {
-                setVisible(false);
-            }
-            setLastScrollY(scrollY);
+            setBottom(scrollY + innerH >= docHeight - 20);
+            setScrolled(scrollY > 20);
         };
         window.addEventListener("scroll", onScroll, { passive: true });
         return () => window.removeEventListener("scroll", onScroll);
@@ -51,7 +44,8 @@ export const Webring = () => {
                 "fixed z-50 bottom-2 mdx:left-1/2 left-1/2 mdx:bottom-5 -translate-x-1/2 justify-center items-center " +
                 "flex bg-black outline-1 outline-neutral-300 rounded-full w-full max-w-[calc(100%-5px)] " +
                 "text-md mdx:w-3xl p-5 mdx:gap-10 mdx:text-2xl transition duration-300 ease-in-out" +
-                (visible ? "translate-y-0" : "absolute translate-y-50 mdx:translate-y-0")
+                (scrolled ? "translate-y-0" : "absolute translate-y-50 mdx:translate-y-0") +
+                (bottom ? "absolute translate-y-50 mdx:translate-y-50" : "translate-y-0 mdx:translate-y-0")
         }>
             <a id="prevurl" className="flex-1 font-mono text-neutral-300" href={prev?.url ?? "https://webring.otomir23.me/30/prev"}> &lt; {prev?.name ?? "prev"} </a>
             <a className="font-mono mdx:text-3xl text-center" href="https://webring.otomir23.me/"> Webring</a>
