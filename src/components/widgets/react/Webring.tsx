7aac9044f5cb3e4d1e31c3696fb0f1599fefa458
glutesha <mail@glutesha.me>
2026-08-27 18:30:21 +0300

    feat(webring): style otoring a lil bit more

--- src/components/widgets/react/Webring.tsx
+++ src/components/widgets/react/Webring.tsx
@@ -72,7 +72,7 @@ export const Webring = ({ prev, next }: entryProps) => {
           </div>
         </a>
         <a
-          className="font-mono mdx:text-2xl font-bold text-center outline-1 py-1 px-4 rounded-full hover:shadow-neutral-300 transition duration-150 hover:-translate-y-1 hover:shadow-md"
+          className="font-mono mdx:text-2xl font-bold text-center outline-1 py-1 px-4 rounded-full transition duration-150 active:shadow-neutral-600 active:-translate-y-0.5 hover:shadow-neutral-300 hover:-translate-y-1 hover:shadow-md"
           href="https://webring.otomir23.me/"
         >
           Otoring
